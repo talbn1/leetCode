3f81095f4b18c7d408e10d3816bb41256abc5bba
Tal <talbn1@gmail.com>
2020-10-07 13:25:36 +0300

    first commit

---
+++ src/com/talbn1/strings/ReorderDataInLogFilesMAIN.java
@@ -0,0 +1,20 @@
+package com.talbn1.strings;
+
+/**
+ * @author talbn on 10/7/2020
+ **/
+public class ReorderDataInLogFilesMAIN {
+    
+    public static void main(String[] args) {
+        
+        String[] logs = logs = new String[]{"dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3" +
+                " art zero"};
+        
+        ReorderDataInLogFiles reorderDataInLogFiles =
+                new com.talbn1.strings.ReorderDataInLogFiles();
+        
+        
+        reorderDataInLogFiles.reorderLogFiles(logs);
+    }
+    
+}
